cd9ade475784f9586f511e64553d6d3f88af0768
Антон Глоба <a.globa@otdelit.ru>
2017-04-24 10:28:02 +0500

    First commit

---
+++ ss_data.ps1
@@ -0,0 +1,139 @@
+﻿$qtype = $args[0]
+
+if ($args.Count -lt 3) {
+    echo "There is no arguments!"
+    exit
+}
+
+switch ($qtype) {
+    "pool" {
+        $qparam = $args[1]
+        $qid = $args[2]
+        
+        $sp = Get-StoragePool -UniqueId "$qid"
+        if (-not $sp) {
+            echo "Object not found!"
+            exit
+        }
+
+        switch ($qparam) {
+            "size" { echo ($sp.Size / 1 ) }
+            "allocated" { echo ($sp.AllocatedSize / 1) }
+            "opstatus" { echo $sp.OperationalStatus }
+            "healthstatus" { echo $sp.HealthStatus }
+        }
+
+    }
+    "enclosure" {
+        $qparam = $args[1]
+        $qid = $args[2]
+
+        $en = Get-StorageEnclosure -UniqueId "$qid"
+
+        if (-not $en) {
+            echo "Object not found!"
+            exit
+        }
+
+        switch ($qparam) {
+            "firmware" { echo $en.FirmwareVersion }
+            "slots" { echo $en.NumberOfSlots }
+            "healthstatus" { echo $en.HealthStatus }
+            "opstatus" { echo $en.OperationalStatus }
+        }
+
+    }
+    "vdisks" {
+        $qparam = $args[1]
+        $qid = $args[2]
+
+        $vd = Get-VirtualDisk -UniqueId "$qid"
+
+        if (-not $vd) {
+            echo "Object not found!"
+            exit
+        }
+
+        switch ($qparam) {
+            "storagepool" { 
+                $sp = Get-StoragePool -VirtualDisk $vd
+                echo $sp.FriendlyName
+            }
+            "size" { echo ($vd.Size / 1) }
+            "writecachesize" { echo ($vd.WriteCacheSize / 1) }
+            "istiered" { if ($vd.IsTiered) { echo 1 } else {echo 0 } }
+            "ssdtiersize" { 
+                if ($vd.IsTiered) { 
+                    $StorageTiers = $vd  | Get-StorageTier
+                    echo (($StorageTiers | Where-Object {$_.MediaType -eq 'SSD'}).Size / 1)
+
+                } else {
+                    echo 0
+                }
+             }
+
+             "hddtiersize" {
+                if ($vd.IsTiered) {
+                    $StorageTiers = $vd  | Get-StorageTier
+                    echo (($StorageTiers | Where-Object {$_.MediaType -eq 'HDD'}).Size / 1)
+                } else {
+                    echo 0
+                }
+             }
+
+             "opstatus" { echo $vd.OperationalStatus }
+             "healthstatus" { echo $vd.HealthStatus }
+             "resiliency" { echo $vd.ResiliencySettingName }
+             "datacopyes" { echo $vd.NumberOfDataCopies }
+             "columns" { echo $vd.NumberofColumns }
+
+        }
+
+    }
+    "pdisks" {
+        $qparam = $args[1]
+        $qid = $args[2]
+
+        $pd = Get-PhysicalDisk -UniqueId "$qid"
+
+        if ($qparam -in ("readlatency","writelatency","readerrors","writeerrors","poweronhours","temperature","temperaturemax")) {
+            $opsdata = Get-StorageReliabilityCounter -PhysicalDisk $pd
+        }
+        
+
+        if (-not $pd) {
+            echo "Object not found!"
+            exit
+        }
+
+        switch ($qparam) {
+            "manufacturer" { echo $pd.Manufacturer }
+            "model" { echo $pd.Model }
+            "slot" { echo $pd.SlotNumber }
+            "enclosureid" { 
+                $findstrg = $pd.PhysicalLocation -match '[0-9a-z]{16}'
+                $EnslosureID = $matches[0]
+                echo ($EnslosureID)
+             }
+            "mediatype" { echo $pd.MediaType }
+            "size" { echo ($pd.Size / 1) }
+            "firmware" { echo $pd.FirmwareVersion }
+            "usage" { echo $pd.Usage }
+            "opstatus" { echo $pd.OperationalStatus }
+            "healthstatus" { echo $pd.HealthStatus }
+            "readlatency" { echo $opsdata.ReadLatencyMax }
+            "writelatency" { echo $opsdata.WriteLatencyMax }
+            "readerrors" { if ( $opsdata.ReadErrorsTotal -eq 0 -Or $opsdata.ReadErrorsTotal -gt 0 ) { echo $opsdata.ReadErrorsTotal } else { echo 0 } }
+            "writeerrors" { if ( $opsdata.WriteErrorsTotal -eq 0 -Or $opsdata.WriteErrorsTotal -gt 0 ) { echo $opsdata.ReadErrorsTotal } else { echo 0 } }
+            "poweronhours" { echo $opsdata.PowerOnHours }
+            "temperature" { echo $opsdata.Temperature }
+            "temperaturemax" { echo $opsdata.TemperatureMax }
+        }
+
+    }
+
+    default {
+        echo "Query type is not walid!"
+        exit
+    }
+}
---
+++ ss_discovery.ps1
@@ -0,0 +1,64 @@
+﻿$dtype = $args[0]
+
+
+switch ($dtype) {
+    "pool" {
+        $StoragePools = Get-StoragePool | Where-Object {$_.FriendlyName -ne 'Primordial'}
+
+        echo "{`n `"data`":[`n"
+        foreach ($sp in $StoragePools) {
+            $line = "{ `n`"{#POOL}`":`"" + $sp.FriendlyName + "`",`n`"{#PDN}`":`"" + $sp.UniqueId + "`",`n`"{#PH}`":`"0`"`n}`n,"
+            echo $line
+        }
+
+        echo "{`n`"{#POOL}`":`"PLACEHOLDER`",`n`"{#PDN}`":`"PLACEHOLDER`",`n`"{#PH}`":`"1`"`n}`n"
+        echo " ]`n}"
+    }
+
+    "enclosure" {
+        $Enclosures = Get-StorageEnclosure | select *
+        echo "{`n `"data`":[`n"
+        foreach ($en in $Enclosures) {
+            $line = "{ `n`"{#ENCLOSURE}`":`"" + $en.FriendlyName + "`",`n`"{#EDN}`":`"" + $en.UniqueId + "`",`n`"{#PH}`":`"0`"`n}`n,"
+            echo $line    
+        }
+        echo "{`n`"{#ENCLOSURE}`":`"PLACEHOLDER`",`n`"{#EDN}`":`"PLACEHOLDER`",`n`"{#PH}`":`"1`"`n}`n"
+        echo " ]`n}"    
+    }
+    
+    "vdisks" {
+        $StoragePools = Get-StoragePool
+        echo "{`n `"data`":[`n"
+        foreach ($sp in $StoragePools)
+        {
+            $VirtualDisks = Get-VirtualDisk -StoragePool $sp
+            foreach ($vd in $VirtualDisks)
+            {
+           
+                $line = "{ `n`"{#VDISK}`":`"" + $vd.FriendlyName + "`",`n`"{#VDN}`":`"" + $vd.UniqueId + "`",`n`"{#PH}`":`"0`"`n}`n,"
+                echo $line
+            }      
+
+        }  
+
+        echo "{`n`"{#VDISK}`":`"PLACEHOLDER`",`n`"{#VDN}`":`"PLACEHOLDER`",`n`"{#PH}`":`"1`"`n}`n"
+        echo " ]`n}"     
+    }
+
+    "pdisks" {
+        $pdisks = Get-PhysicalDisk | where {($_.canpool) -or ($_.CannotPoolReason -match 'In a Pool')}
+        echo "{`n `"data`":[`n"
+        foreach ($pd in $pdisks)
+        {
+
+
+            $line = "{ `n`"{#PDISK}`":`"" + $pd.SerialNumber + "`",`n`"{#PDN}`":`"" + $pd.UniqueId + "`",`n`"{#PH}`":`"0`"`n}`n,"
+            echo $line
+
+        }
+        echo "{`n`"{#PDISK}`":`"PLACEHOLDER`",`n`"{#PH}`":`"1`"`n}`n"
+        echo " ]`n}"     
+    }
+}
+    
+
